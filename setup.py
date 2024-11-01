0bd249d3dc1ce085e12d577d6b5833eac8016371
serber1990 <serber1990@pm.me>
2024-11-01 23:03:05 +0100

    Update setup.py to version 0.2 with PyPI and GitHub links

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="colorize_term",
-    version="0.1",
+    name="colorize-term",
+    version="0.2",
     description="A simple library to add ANSI color codes to terminal text",
-    long_description=open("README.md").read(),  # Optional, include your README for PyPI description
+    long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     author="Serber1990",
     author_email="serber1990@pm.me",
+    url="https://github.com/serber1990/colorize-term",  # Link to your GitHub repository
+    project_urls={
+        "Bug Tracker": "https://github.com/serber1990/colorize-term/issues",
+        "Documentation": "https://github.com/serber1990/colorize-term",
+        "Source Code": "https://github.com/serber1990/colorize-term",
+    },
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
