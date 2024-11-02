7a6993e57b25678e0acd3ca5571fd31c699d87c3
Serber1990 <39016027+serber1990@users.noreply.github.com>
2024-11-02 01:01:05 +0100

    Update setup.py
    New Name

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="colorize-term",
-    version="0.2",
+    name="shellcolorize",
+    version="0.1",
     description="A simple library to add ANSI color codes to terminal text",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     author="Serber1990",
     author_email="serber1990@pm.me",
-    url="https://github.com/serber1990/colorize-term",  # Link to your GitHub repository
+    url="https://github.com/serber1990/shell-colorize",  # Link to your GitHub repository
     project_urls={
-        "Bug Tracker": "https://github.com/serber1990/colorize-term/issues",
-        "Documentation": "https://github.com/serber1990/colorize-term",
-        "Source Code": "https://github.com/serber1990/colorize-term",
+        "Bug Tracker": "https://github.com/serber1990/shell-colorize/issues",
+        "Documentation": "https://github.com/serber1990/shell-colorize",
+        "Source Code": "https://github.com/serber1990/shell-colorize",
     },
     packages=find_packages(),
     classifiers=[
